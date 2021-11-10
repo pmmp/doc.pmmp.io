8e62ef2854b889c1b6956dcf61e5db86907ad111
Dylan K. Taylor <dktapps@pmmp.io>
2021-11-10 01:29:50 +0000

    conf: fix styling on readthedocs this didn't match local style and made it hard to debug why the fucking bullets weren't working on readthedocs.

--- source/conf.py
+++ source/conf.py
@@ -95,28 +95,13 @@ pygments_style = 'sphinx'
 
 
 # -- Options for HTML output ----------------------------------------------
-# on_rtd is whether we are on readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-	import sphinx_rtd_theme
-	html_theme = 'sphinx_rtd_theme'
-	html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-	def setup(app):
-		#app.add_javascript("custom.js")
-		app.add_css_file('theme_overrides.css')
-else:
-    # Override default css to get a larger width for ReadTheDoc build
-    html_context = {
-        'css_files': [
-            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-            '_static/theme_overrides.css',
-        ],
-    }
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
+
+html_css_files = [
+    'theme_overrides.css',
+]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
